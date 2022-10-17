a298460d1b6ac2b93cb5becf2951c9b94b8e565c
Kavya-gopi <113252110+Kavya-gopi@users.noreply.github.com>
2022-10-17 14:41:51 +0530

    Delete sql.py

--- Assignments/Team Lead/Assignment 2/sql.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('students.db')
-print('opened database successfully')
-
-conn.execute('CREATE TABLE students (name TEXT, email TEXT, mobile TEXT, city TEXT, state TEXT, country TEXT, password TEXT)')
-print("Table Created Successfully")
-conn.close()  
