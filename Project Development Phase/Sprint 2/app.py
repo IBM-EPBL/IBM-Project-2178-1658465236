12ede73235a89fceca61796640df2fb9d312345d
Kavya-gopi <113252110+Kavya-gopi@users.noreply.github.com>
2022-11-16 13:44:26 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 2/app.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template
+
+app=Flask('__name__')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+    
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+    
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
